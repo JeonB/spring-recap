fc13135e3b752aeed5cd38bb8d36b6f6fc151e94
JeonB <newjeonb@gmail.com>
2024-07-19 11:24:46 +0900

    part1 - 상품목록 페이지 만들기

--- src/main/java/com/example/springrecap/SpringRecapApplication.java
+++ src/main/java/com/example/springrecap/SpringRecapApplication.java
@@ -9,7 +9,5 @@ public class SpringRecapApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRecapApplication.class, args);
-        String lover = "고말숙";
-        System.out.println(lover + " 사랑해");
     }
 }
---
+++ src/main/java/com/example/springrecap/controller/ItemController.java
@@ -0,0 +1,8 @@
+package com.example.springrecap.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ItemController {
+
+}
