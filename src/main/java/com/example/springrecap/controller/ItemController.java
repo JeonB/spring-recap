c884fa33a78bd04ff423243c1b5463d5b2230850
JeonB <newjeonb@gmail.com>
2024-07-22 13:58:04 +0900

    DB 데이터 출력하려면

--- src/main/java/com/example/springrecap/controller/ItemController.java
+++ src/main/java/com/example/springrecap/controller/ItemController.java
@@ -1,19 +1,35 @@
 package com.example.springrecap.controller;
 
-import java.util.Arrays;
+import com.example.springrecap.entity.Item;
+import com.example.springrecap.entity.ItemRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
 
+    private final ItemRepository itemRepository;
+
+    /* lombok 쓰기 싫으면 생성자 자동 생성 ㄱㄱ */
+//    @Autowired
+//    public ItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
 
     @GetMapping("/list")
     String list(Model model){
-        String[] items = {"apple", "banana", "kiwi"};
-        model.addAttribute("items", String.join(", ", items).replace("[", "").replace("]", ""));
+        List<Item> result = itemRepository.findAll();
+        StringBuilder titles = new StringBuilder();
+        for(Item item : result){
+            titles.append(item.title).append(" ");
+        }
+        model.addAttribute("items", titles.toString());
         return "list";
     }
 }
