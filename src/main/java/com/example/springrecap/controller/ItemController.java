a25c0e36627a64362d6d484479032f13da709fc5
JeonB <newjeonb@gmail.com>
2024-07-27 17:52:45 +0900

    코딩 애플 part 2 - 상품 추가 기능1

--- src/main/java/com/example/springrecap/controller/ItemController.java
+++ src/main/java/com/example/springrecap/controller/ItemController.java
@@ -2,13 +2,16 @@ package com.example.springrecap.controller;
 
 import com.example.springrecap.entity.Item;
 import com.example.springrecap.entity.ItemRepository;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,7 +28,28 @@ public class ItemController {
     @GetMapping("/list")
     String list(Model model){
         List<Item> result = itemRepository.findAll();
+        System.out.println(result);
         model.addAttribute("items", result);
         return "list";
+    }
+
+    @GetMapping("/write")
+    String write(){
+        return "write";
+    }
+
+    @PostMapping("/item")
+    String save(@ModelAttribute Item item){
+//        String save(@RequestParam Map formData){
+//        Map data = formData;
+//
+//        String title = (String) data.get("title");
+//        Integer price = Integer.parseInt((String) formData.get("price"));
+//
+//        Item item = new Item();
+//        item.setTitle(title);
+//        item.setPrice(price);
+        itemRepository.save(item);
+        return "redirect:/list";
     }
 }
--- src/main/java/com/example/springrecap/entity/Item.java
+++ src/main/java/com/example/springrecap/entity/Item.java
@@ -6,11 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @ToString
 @Getter
+@Setter
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
